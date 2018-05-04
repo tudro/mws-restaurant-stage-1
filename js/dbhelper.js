a6d466f9f7126c2b061463fe0336bfdf1b8fc48a
tudor rogojanu <tudor.rogojanu@gmail.com>
2018-05-04 11:53:14 +0300

    feat: Pull json data from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,33 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    //const port = 8000; // Change this to your server port
+    //return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
+    fetch(DBHelper.DATABASE_URL)
+      .then(response => {
+        return response.json();
+      })
+      .then(restaurants => restaurants.map(restaurant => {
+        if ('photograph' in restaurant) {
+          restaurant.photograph += '.jpg';
+        } else {
+          restaurant.photograph = 'default.jpg';
+        }
+        return restaurant;
+      }))
+      .then(restaurants => callback(null, restaurants))
+      .catch(e => {
+        const error = (`Request failed: ${e}`);
         callback(error, null);
-      }
-    };
-    xhr.send();
+      });
   }
 
   /**
